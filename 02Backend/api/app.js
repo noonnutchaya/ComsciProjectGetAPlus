ed5d9b196b27a3bb2fddabb0d87142529164a11e
noonnutchaya <chocola.2542@hotmail.com>
2021-02-08 15:11:29 +0700

    backend

--- 02Backend/api/app.js
+++ 02Backend/api/app.js
@@ -5,9 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/testAPI');
-var pdfRouter = require('./routes/testPDF');
+
 
 var app = express();
 
@@ -23,9 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testAPI', apiRouter);
-app.use('/testPDF', pdfRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ 02Backend/api/routes/cal4Page.js
@@ -0,0 +1,132 @@
+var express = require("express");
+var router = express.Router();
+var fs = require("fs");
+const Jimp = require("jimp");
+const brain = require("brain.js");
+var pdf2img = require("pdf-img-convert");
+
+router.post("/", async function(req, res, next) {
+  const { weight, quantity, color, urlfile } = req.body;
+  let [allPage, typeFile, realPrice, count] = [1, 0, 0, 0];
+  let [white, lightTone, darkTone] = [0, 0, 0];
+  let [percentWhite, percentLightTone, percentDarkTone] = [0, 0, 0];
+
+  console.log("--- 4 Page ---");
+  console.log(
+    "req -> " +
+      "color: " +
+      color +
+      " - quantity: " +
+      quantity +
+      " - weight: " +
+      weight
+  );
+
+  if (urlfile.includes("pdf") || urlfile.includes("PDF")) {
+    typeFile = 1;
+    console.log("File Type: PDF");
+    let pdfArray = await pdf2img.convert(urlfile);
+    console.log("saving");
+    allPage = pdfArray.length;
+    for (i = 0; i < pdfArray.length; i++) {
+      fs.writeFile("output" + (i + 1) + ".png", pdfArray[i], function(error) {
+        if (error) {
+          console.error("Error: " + error);
+        }
+      });
+    }
+  } else {
+    console.log("File Type: Image");
+  }
+
+  // training data - brain.js
+  const network = new brain.NeuralNetwork();
+  let fileName = "./routes/trainingData.JSON";
+  let trainingData = await JSON.parse(fs.readFileSync(fileName, "utf8"));
+  network.train(trainingData);
+
+  if (color == "color") {
+    console.log("color printing");
+    for (i = 1; i <= allPage; i++) {
+      let image;
+
+      if (typeFile == 1) {
+        image = await Jimp.read("./output" + i + ".png"); // pdf
+      } else {
+        image = await Jimp.read(urlfile); // image
+      }
+
+      image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+        const thisColor = {
+          r: image.bitmap.data[idx + 0],
+          g: image.bitmap.data[idx + 1],
+          b: image.bitmap.data[idx + 2]
+        };
+        count += 1;
+
+        if (thisColor.r >= 250 && thisColor.g >= 250 && thisColor.b >= 250) {
+          white += 1;
+        } else {
+          if (thisColor.r >= 200 || thisColor.g >= 200 || thisColor.b >= 200) {
+            lightTone += 1;
+          } else {
+            darkTone += 1;
+          }
+        }
+      });
+
+      percentWhite = white / count;
+      percentDarkTone = darkTone / count;
+      percentLightTone = lightTone / count;
+
+      let priceProb = network.run([
+        percentWhite,
+        percentDarkTone,
+        percentLightTone
+      ]);
+
+      realPrice = realPrice + priceProb * 15;
+      console.log("Calculate Page - " + i + " - Price: " + priceProb * 15);
+      count = 0;
+      white = 0;
+      lightTone = 0;
+      darkTone = 0;
+    }
+  } else {
+    console.log("black printing");
+    realPrice = allPage;
+  }
+
+  console.log("PriceWithDot: " + realPrice + " - " + color);
+
+  int_part = Math.trunc(realPrice);
+  float_part = Number((realPrice - int_part).toFixed(2));
+  if (float_part < 0.5) {
+    realPrice = int_part;
+  } else {
+    realPrice = int_part + 1;
+  }
+
+  console.log("check: " + realPrice);
+
+  if (weight >= 110 && weight <= 130) {
+    console.log("+1");
+    realPrice = realPrice + allPage; // 1 baht. per sheet
+  } else if (weight == 150) {
+    console.log("+3");
+    realPrice = realPrice + allPage * 3; // 3 baht. per sheet
+  } else if (weight < 110) {
+    console.log("+0");
+    realPrice = realPrice;
+  }
+
+  console.log("check 2: " + realPrice);
+  console.log("check 3 quantity: " + quantity);
+  realPrice = realPrice * quantity;
+
+  console.log("totalPrice: " + realPrice);
+  console.log("-----------------------------------");
+  // res.send(realPrice.toString());
+});
+
+module.exports = router;
